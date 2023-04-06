e326fcfff1e4c5671895f353c3c0dfa0d896f07e
Massimo Ercolani <massimo.ercolani@parloa.com>
2023-04-06 19:37:35 +0200

    return 204

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import { Handler } from '@netlify/functions';
 
 const handler: Handler = async () => {
   return {
-    statusCode: 200,
-    body: 'I am being developed as we speak, but bare with me I will be live soon!',
+    statusCode: 204,
   };
 };
 
