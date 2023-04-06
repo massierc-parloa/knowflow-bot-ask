20bce83face0395cba16fe8581006ab70ccbee3d
Massimo Ercolani <massimo.ercolani@parloa.com>
2023-04-06 19:35:42 +0200

    implement chain

--- src/ask.ts
+++ src/ask.ts
@@ -1,21 +1,45 @@
 import { TextLoader } from 'langchain/document_loaders';
 import { CharacterTextSplitter } from 'langchain/text_splitter';
+import { HNSWLib } from 'langchain/vectorstores';
+import { OpenAIEmbeddings } from 'langchain/embeddings';
+import { OpenAI } from 'langchain/llms';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const files = [
+const FILES = [
   `${__dirname}/../data/eng/faqs.txt`,
   `${__dirname}/../data/eng/aktuelle-marktlage.txt`,
   `${__dirname}/../data/eng/gaspreisbremse.txt`,
   `${__dirname}/../data/eng/strompreisbremse.txt`,
 ];
 
-const loaders = files.map(file => new TextLoader(file));
-const docs = loaders.map(loader => loader.load());
-
 const splitter = new CharacterTextSplitter({
   chunkSize: 1000,
   chunkOverlap: 200,
 });
 
-const splitDocs = docs.map(
-  async doc => await splitter.splitDocuments(await doc)
-);
+const model = new OpenAI({});
+
+export const ask = async (question: string) => {
+  const loaders = FILES.map(file => new TextLoader(file));
+  const docs = await Promise.all(loaders.map(loader => loader.load()));
+  const splitDocs = await Promise.all(
+    docs.map(doc => splitter.splitDocuments(doc))
+  );
+
+  const vectorStore = await HNSWLib.fromDocuments(
+    splitDocs.flat(),
+    new OpenAIEmbeddings()
+  );
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever()
+  );
+
+  const res = await chain.call({
+    question,
+    chat_history: [],
+  });
+
+  return res.text;
+};
